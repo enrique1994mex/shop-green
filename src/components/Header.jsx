7fcff49ac7abc9f4a8d913bd114d131013daadae
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-04-28 12:55:20 -0600

    add styles of navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
 import style from '../styles/components/header.module.scss'
+import profile from '../assets/profile.svg'
+import heart from '../assets/heart.svg'
+import shopping from '../assets/shopping-cart.svg'
 const Header = () => {
 	return (
 		<nav className={style.navbar}>
 			<p>Eco-store</p>
-			<button className='profile'>
-				<svg></svg>
-			</button>
-			<button className='wishlist'>
-				<svg></svg>
-			</button>
-			<button className='cart'>
-				<svg></svg>
-			</button>
+			<div className={style.icons}>
+				<button className='profile'>
+					<img src={profile} height='30' width='30' />
+				</button>
+				<button className='wishlist'>
+					<img src={heart} height='30' width='30' />
+				</button>
+				<button className='cart'>
+					<img src={shopping} height='30' width='30' />
+				</button>
+			</div>
 		</nav>
 	)
 }
