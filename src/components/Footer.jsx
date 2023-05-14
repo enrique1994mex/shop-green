a198d2b5fa7c9e14db67612cc15d5351006cf235
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-05-13 19:04:01 -0600

    add main and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
+import instragram from '../assets/img/instagram.png'
+import facebook from '../assets/img/facebook.png'
+import twitter from '../assets/img/twitter.png'
 import style from '../styles/components/footer.module.scss'
 const Footer = () => {
 	return (
 		<footer className={style.footer}>
 			<h3>Eco-store</h3>
 			<p>Careers</p>
-			<p>Careers</p>
-			<p>Careers</p>
-			<div className='social'>
-				<img src='' alt='' />
+			<p>Terms and conditions</p>
+			<p>Eco-store inc.</p>
+			<div className={style.social}>
+				<img src={instragram} alt='Instagram' />
+				<img src={facebook} alt='Facebook' />
+				<img src={twitter} alt='Twitter' />
 			</div>
 		</footer>
 	)
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,58 +1,198 @@
+import imageMain from '../assets/img/main.jpg'
+import productSvg from '../assets/heart.svg'
+import product from '../assets/img/brush.jpg'
+import earth from '../assets/img/earth.png'
+import ecology from '../assets/img/ecology.png'
+import gallery1 from '../assets/img/gallery-1.jpg'
+import gallery2 from '../assets/img/gallery-2.jpg'
+import gallery3 from '../assets/img/gallery-3.jpg'
+import map from '../assets/img/map.png'
 import style from '../styles/components/main.module.scss'
 const Main = () => {
 	return (
 		<main className={style.main}>
 			<article>
-				<div className='impact'>
+				<div className={style.impact}>
 					<h1>Impacto ecológico</h1>
 					<p>
-						Impacto Ecológico Debido al impacto ambiental siendo las acciones
-						humanas las que modifican el ambiente, Eco-store se compromete a
-						elaborar sus productos de manera artesanal y con materiales que
-						favorecen al cuidado del medio ambiente. Conoce más
+						Debido al impacto ambiental siendo las acciones <br />
+						humanas las que modifican el ambiente, Eco-store <br /> se
+						compromete a elaborar sus productos de manera <br />
+						artesanal y con materiales que favorecen al cuidado <br /> del medio
+						ambiente. Conoce más
 					</p>
 					<button>Conoce más</button>
 				</div>
-				<div className='image'>
-					<img src='' alt='' />
+				<div className={style.image}>
+					<img src={imageMain} alt='Imagen principal' />
 				</div>
 			</article>
-			<section className='product'>
-				<div className='healthcare'>
+			<section className={style.product}>
+				<div className={style.healthcare}>
 					<h2>Cuidado de la salud</h2>
+					<div className={style.card}>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+					</div>
 				</div>
-				<div className='furniture'>
+				<div className={style.furniture}>
 					<h2>Decoración del hogar</h2>
+					<div className={style.card}>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+						<div className={style.productCard}>
+							<div className={style.icon}>
+								<img src={productSvg} alt='Icono de producto' />
+							</div>
+							<div className={style.productImage}>
+								<img src={product} alt='Cepillo' />
+							</div>
+							<h3>Cepillo de bambú</h3>
+							<p>Paquete de 2 unidades</p>
+						</div>
+					</div>
 				</div>
 			</section>
 			<section className='info'>
-				<div className='about-us'>
+				<div className={style.aboutUs}>
 					<h2>¿Por qué nosotros?</h2>
-					<div className='earth'>
-						<img src='' alt='' />
-					</div>
-					<div className='innovation'>
-						<img src='' alt='' />
+					<div className={style.mision}>
+						<div className={style.earth}>
+							<img src={earth} alt='Tierra' />
+							<p>
+								Compromiso con el medio ambiente garantizando el uso de
+								materiales 100% reciclables.
+							</p>
+						</div>
+						<div className={style.innovation}>
+							<img src={ecology} alt='Ecología' />
+							<p>
+								Innovando con estilo el diseño de nuestros muebles es único.
+							</p>
+						</div>
 					</div>
 				</div>
-				<div className='gallery'>
+				<div className={style.gallery}>
 					<h2>Galería de estilos</h2>
-					<div>
-						<img src='' alt='' />
-					</div>
-					<div>
-						<img src='' alt='' />
-					</div>
-					<div>
-						<img src='' alt='' />
+					<div className={style.galleryImages}>
+						<img src={gallery1} alt='Galería 1' />
+						<img src={gallery2} alt='Galería 2' />
+						<img src={gallery3} alt='Galería 3' />
 					</div>
 				</div>
-				<div className='location'>
+				<div className={style.location}>
 					<h2>Encuéntranos</h2>
-					<div className='map'>
-						<img src='' alt='' />
+					<div className={style.map}>
+						<img src={map} alt='Mapa' />
 					</div>
-					<p></p>
+					<h3>Centro Mayor Centro Comercial</h3>
+					<p>Cl. 38A Sur #34d-51, Bogotá, Colombia</p>
 				</div>
 			</section>
 		</main>
